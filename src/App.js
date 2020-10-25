9d653ee2fc1725c9e8d8cb2499bb53c9d76c46ce
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-10-25 14:41:29 +0100

    update App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import { Component } from 'react';
 import { csvParse } from 'd3-dsv';
-import dataCsv from './zipcodes_1000';
+import dataCsv from './zipcodes_all';
 import Map from './Components/Map';
 
 const data = csvParse(dataCsv, (d) => {
   return {
-    lon: -d.lon,
+    lon: +d.lon,
     lat: +d.lat,
     zipcode: +d.zipcode,
     city: d.city,
@@ -15,19 +15,12 @@ const data = csvParse(dataCsv, (d) => {
 class App extends Component {
   state = {
     data: data,
-    width: 700,
-    height: 500,
   };
 
   render() {
     // console.log(this.state.data);
-    const city = data.slice(0, 50).map((datum) => <p>{datum.city} </p>);
-    return (
-      <div className="App">
-        <Map data={this.state.data} width={this.state.width} height={this.state.height}></Map>
-        <p>{city}</p>
-      </div>
-    );
+    // const city = data.slice(0, 50).map((datum) => <p>{datum.city} </p>);
+    return <Map data={this.state.data}></Map>;
   }
 }
 
