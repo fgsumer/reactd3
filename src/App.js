1c7b587306e111eec6974ec4b107195bd1a141e4
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-10-25 11:34:28 +0100

    add csvParse

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { csvParse } from 'd3-dsv';
+import dataCsv from './zipcodes_1000';
+import Map from './Components/Map';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = csvParse(dataCsv, (d) => {
+  return {
+    lon: -d.lon,
+    lat: +d.lat,
+    zipcode: +d.zipcode,
+    city: d.city,
+  };
+});
+class App extends Component {
+  state = {
+    data: data,
+    width: 700,
+    height: 500,
+  };
+
+  render() {
+    // console.log(this.state.data);
+    const city = data.slice(0, 50).map((datum) => <p>{datum.city} </p>);
+    return (
+      <div className="App">
+        <Map data={this.state.data} width={this.state.width} height={this.state.height}></Map>
+        <p>{city}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
